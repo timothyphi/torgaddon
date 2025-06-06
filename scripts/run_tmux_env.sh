5c0c000021089e701b7c863d2d0c3912e9b2cd87
Timothy Nguyen <timothy.nguyen.9@icloud.com>
2025-06-06 15:02:01 -0500

    fixed script bug

--- scripts/run_tmux_env.sh
+++ scripts/run_tmux_env.sh
@@ -9,7 +9,7 @@ PROJECT_DIR="$(readlink -f "$SCRIPT_DIR/../")"
 tmux new-session -d -s "$SESSION" -c "$PROJECT_DIR"
 
 tmux rename-window -t "$SESSION:0" 'server'
-tmux send-keys -t "$SESSION:0" 'npm run server' C-m
+tmux send-keys -t "$SESSION:0" 'npm run server' C-m # TODO: update to npm run server:watch when implemented
 
 tmux new-window -t "$SESSION:1" -n 'ts' -c "$PROJECT_DIR"
 tmux send-keys -t "$SESSION:1" 'npm run ts' C-m
@@ -21,6 +21,6 @@ tmux new-window -t "$SESSION:3" -n 'uswds' -c "$PROJECT_DIR"
 tmux send-keys -t "$SESSION:3" 'npm run uswds' C-m
 
 tmux new-window -t "$SESSION:4" -n 'editor' -c "$PROJECT_DIR"
-tmux send-keys -t "$SESSION:3" 'npm run eslint -- .' C-m
+tmux send-keys -t "$SESSION:4" 'npm run eslint -- .' C-m
 
 tmux attach -t "$SESSION"
